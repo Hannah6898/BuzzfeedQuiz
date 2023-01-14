84a3f302a6dd9190ba3620bedf8a4218f84cba6f
Hannah6898 <hannahosibodu68@outlook.com>
2023-01-14 15:28:27 +0000

    Set up project structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import Title from 'components/Title';
+import QuestionBlock from "components/QuestionBlock"
+import QuestionsBlock from "components/QuestionsBlock"
+import AnswerBlock from "components/AnswerBlock"
+
 
 function App() {
   return (
     <div >
+      <Title/>
 
     </div>
   );
---
+++ src/components/QuestionsBlock.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function QuestionsBlock() {
+    return ( <div></div> );
+}
+
+export default QuestionsBlock;
