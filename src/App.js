aa5c55afe1f02654da4f83969d2c15a3cc9b3436
Hannah6898 <hannahosibodu68@outlook.com>
2023-01-14 15:54:54 +0000

    Fetch json data from front-end

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import Title from 'components/Title';
-import QuestionBlock from "components/QuestionBlock"
-import QuestionsBlock from "components/QuestionsBlock"
-import AnswerBlock from "components/AnswerBlock"
+import Title from './components/Title';
+import QuestionBlock from './components/QuestionBlock'
+import QuestionsBlock from "./components/QuestionsBlock"
+import AnswerBlock from "./components/AnswerBlock"
+import {useState, useEffect} from 'react'
 
 
 function App() {
+  const [quiz,setQuiz] = useState(false)
+
+  const fetchData = async()=>{
+    try{
+      const reponse = await fetch('http://localhost:8000/quiz')
+      const json = await reponse.json()
+      console.log(json)
+      setQuiz(json)
+    }catch(err){
+      console.log(err)
+    }
+  }
+
+  useEffect(()=>{
+    fetchData()
+  },[])
+
   return (
     <div >
       <Title/>
